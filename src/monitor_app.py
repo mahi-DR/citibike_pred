9768a082d2adfe2ad628b4a2fc43ea7795e551d1
mahi-DR <desireddy.mahiswar.2002@gmail.com>
2025-05-10 17:59:30 +0530

    Update monitor_app.py

--- src/monitor_app.py
+++ src/monitor_app.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 
 # Function to fetch prediction data from session_state
 def fetch_prediction_data():
-    # Debugging to ensure session state is working
     if 'predictions' in st.session_state:
+        # Debugging to ensure session state is working
         st.write("Predictions found in session_state.")
         return pd.DataFrame(st.session_state['predictions'])
     else:
